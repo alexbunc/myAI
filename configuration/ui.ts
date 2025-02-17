2b9ff3616b4844cf27039bd9a91e319d3b6eff7d
Alexandra Binnie <alexlbinnie@gmail.com>
2025-02-17 15:17:28 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -5,6 +5,6 @@ export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `[Copyright 2025 @ Alex Binnie]`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
 export const PAGE_TITLE: string = `[PAGE TITLE]`;
-export const PAGE_DESCRIPTION: string = `Chat with ${Al} AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Chat with ${Al}, AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
