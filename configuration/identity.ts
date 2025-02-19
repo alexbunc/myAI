dbb700ff3b32fa9fdd97b74b422fcdb4c3a1bf29
Alexandra Binnie <alexlbinnie@gmail.com>
2025-02-19 14:27:05 -0500

    Update identity.ts
    Changed role to real estate/energy expert

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -3,4 +3,4 @@ export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
 export const AI_NAME: string = `Al the AI Bot`;
 export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_ROLE: string = `You are an expert on the real estate and energy industries.`;
